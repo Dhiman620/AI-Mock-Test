3300b23f5c2d3df91a467569ca551bf4c4a2c74d
Dhiman620 <dhimandebsarma2003@gmail.com>
2025-04-25 16:10:54 +0530

    changes: Home Screen Completed

--- src/layouts/main_layout.tsx
+++ src/layouts/main_layout.tsx
@@ -1,23 +1,23 @@
-import { Container } from "@/components/container"
-import Footer from "@/components/footer"
-import Header from "@/components/header"
-import { Outlet } from "react-router"
+import { Container } from "@/components/container";
+import { Footer } from "@/components/footer";
+
+import Header from "@/components/header";
+import { Outlet } from "react-router";
 
 export const MainLayout = () => {
   return (
     <div>
       <div className=" flex flex-col h-screen">
-        <Header/>
+        <Header />
 
         <Container className=" flex-grow">
           <main className=" flex-grow">
-            <Outlet/>
+            <Outlet />
           </main>
         </Container>
-        
 
-        <Footer/>
+        <Footer />
       </div>
     </div>
-  )
-}
+  );
+};
